685dfd2660971136b2f8c040b42b74d7342a41d2
Amarisse <33473123+mari3mmi@users.noreply.github.com>
2018-12-01 08:53:29 -0500

    Revert "Feature/tts audioplayer"

--- client/src/views/Room/Room.js
+++ client/src/views/Room/Room.js
@@ -1,51 +1,15 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
-import ReactAudioPlayer from 'react-audio-player';
 import socketIOClient from "socket.io-client";
 import Swal from 'sweetalert2';
+import axios from 'axios';
 import {Button} from 'semantic-ui-react';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-import * as axios from 'axios';
-
-const accessToken = 'ya29.GltlBhCAmHwtzNTbQfKboVPtq2PW_C3m1rrU7NvI50JSifIc-NuKksiBdnvbHtTXyQUeWeZTuzPEq7VV7iivHMkBy6_-aU01wwtwxBJwjCFHTXyQYSeGw4h6rBXm';
 
 class App extends Component {
 
   state = {
-    base64File :"",
     socket: socketIOClient("http://127.0.0.1:5000/"),
-  }
-
-  buttonClick(){
-    console.log('hi');
-  }
-  otherFunction(){
-    let context = new AudioContext();
-    var base64Buffer = "";
-    let config = {
-      headers: {
-        'Authorization': 'Bearer ' + accessToken
-      }
-    }
-    axios.post('https://texttospeech.googleapis.com/v1beta1/text:synthesize', { 
-      'input':{
-        'text':'I\'ve added the event to your calendar.'
-      },
-      'voice':{
-        'languageCode':'en-gb',
-        'name':'en-GB-Standard-A',
-        'ssmlGender':'FEMALE'
-      },
-      'audioConfig':{
-        'audioEncoding':'MP3'
-      }
-    }, config)
-    .then((response) => {
-      this.setState({base64File: response.data.audioContent});
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
   }
 
   joinRoom(id, name) {
@@ -98,6 +62,8 @@ console.log(name);
     });
   }
 
+
+
   render() {
     
     return (  
@@ -109,13 +75,6 @@ console.log(name);
             <Button>Copy to clipboard</Button>
           </CopyToClipboard>
             <Header as='h1'> {this.state.name} Room</Header>
-            <button onClick={() => this.otherFunction()}>Click here to fetch</button>
-            <ReactAudioPlayer
-              src={'data:audio/mp3;base64,'+this.state.base64File}
-              autoPlay
-              controls
-            />
-            {this.state.base64File}
         </div>
     );
   }
